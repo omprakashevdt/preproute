1079ee187c05dce0d18e56081aa691a249e332de
omprakashevdt <omprakash.evdt@gmail.com>
2026-02-11 22:13:18 +0530

    update base url for prod

--- src/redux/apiClient.ts
+++ src/redux/apiClient.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_URL,
+  baseURL: import.meta.env.DEV
+    ? import.meta.env.VITE_URL
+    : import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
